a30c8c04c09fb6d7b4fdc7b2de0546eb1e766267
OROU DRAMANI <faizouoroudramani@gmail.com>
2021-01-12 15:55:58 +0100

    Content, achevé

--- dramani-app/src/ContentComponent/RightSideBar.js
+++ dramani-app/src/ContentComponent/RightSideBar.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './RightSideBar.css';
 import { FiSettings } from "react-icons/fi";
 import { IoRocketSharp, IoPencilSharp } from "react-icons/io5";
-import { SiPrettier } from "react-icons/si";
+import { SiPrettier, SiAdguard} from "react-icons/si";
 import { FcFlashOn, FcGallery } from "react-icons/fc";
 import { FiMinus } from "react-icons/fi";
-import { FaBirthdayCake, FaLanguage } from "react-icons/fa";
-import { GiStarSattelites, GiLovers } from "react-icons/gi";
-import { BsHeart } from "react-icons/bs";
+import { FaBirthdayCake, FaLanguage, FaHandPeace, FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
+import { GiStarSattelites, GiLovers, GiEnvelope } from "react-icons/gi";
+import { BsHeart, BsPhone } from "react-icons/bs";
 import { BiChalkboard } from "react-icons/bi";
 import { MdWork, MdSchool } from "react-icons/md";
+import { } from "react-icons/si";
 
 const RightSideBar =()=> {
      return (
@@ -84,14 +85,131 @@ const RightSideBar =()=> {
             <hr/>
             <div className="profileVerification">
                 <h5>Vérification</h5>
-                <div>
-
-                </div>
+                    <div className="itemGuardVerification">
+                            <div className="guardIcon">
+                                <SiAdguard size="2rem" color="white"/>
+                                <span>0/3</span>
+                            </div>
+                            <div>
+                                Un profil vérifié te permet de faire plus de rencontres ! Encore 3 vérifications pour obtenir le statut vérifié
+                            </div>    
+                    </div>
+              
 
                 <div className="itemVerification">
-                    <div className="itemVerification">
 
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <FaHandPeace size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Photo de profile</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
                     </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <SiPrettier size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Premium</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <FaFacebookF size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Facebook</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <GiEnvelope size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Windows Live</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <BsPhone size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Numéro de téléphone</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <FaTwitter size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Twitter</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <FaLinkedinIn size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Linkedin</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
+                    <div className="eachItemVerification">
+                        <div className="oneItemVerification">
+                             <div className="iconManagement">
+                                <span></span>
+                                <FaInstagram size="2rem" color="white"/> 
+                             </div>
+                            <div className="itemVerificationText" >
+                                <span>Instagram</span>
+                                <span>Pas vérifié</span>
+                            </div>
+                        </div>
+                        <button className="verificationButton">Vérifier</button>
+                    </div>
+
                 </div>
 
 
